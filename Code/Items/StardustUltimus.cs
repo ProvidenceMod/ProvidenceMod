333eb41349457db8f52a122515d1ade2da3475e1
SnowyStarfall <65053311+SnowyStarfall@users.noreply.github.com>
2020-05-10 14:35:21 -0500

    Changing StardustUltimus. Added StardustUltimusGlowmask.png. Added SolarCultist.png. Added SolarCultist_Head.png.

--- Code/Items/StardustUltimus.cs
+++ Code/Items/StardustUltimus.cs
@@ -3,6 +3,7 @@ using Terraria.ModLoader;
 using Terraria;
 using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace UnbiddenMod.Code.Items
 {
@@ -42,10 +43,16 @@ namespace UnbiddenMod.Code.Items
             }
             return false;
         }
+        
+        public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float  scale, int whoAmI) 	
+        {
+        	Texture2D texture = mod.GetTexture("StardustUltimusGlowmask");
+	        spriteBatch.Draw(texture, new Vector2(item.position.X - Main.screenPosition.X + item.width * 0.5f, item.position.Y - Main.screenPosition.Y + item.height - texture.Height * 0.5f + 2f), new Rectangle(0, 0, texture.Width, texture.Height), Color.White, rotation, texture.Size() * 0.5f, scale, SpriteEffects.None, 0f);
+        }
 
         public override void AddRecipes()
         {
-            // Recipes here. See Basic Recipe Guide
+            // Recipes here. See Basic Recipe Guide2
             ModRecipe recipe = new ModRecipe(mod);
 
             recipe.AddIngredient(ItemID.DirtBlock, 10); //Adds ingredients

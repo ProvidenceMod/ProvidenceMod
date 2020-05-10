c01688709126818bfbb02c5d105c0d52ab9583f8
SnowyStarfall <65053311+SnowyStarfall@users.noreply.github.com>
2020-05-10 18:10:14 -0500

    Update StardustUltimus.cs

--- Code/Items/StardustUltimus.cs
+++ Code/Items/StardustUltimus.cs
@@ -9,6 +9,7 @@ namespace UnbiddenMod.Code.Items
 {
     public class StardustUltimus : ModItem
     {
+
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Stardust Ultimus");
@@ -42,12 +43,6 @@ namespace UnbiddenMod.Code.Items
                 Projectile.NewProjectile(position.X, position.Y, vX, vY, type, damage, knockBack, Main.myPlayer);
             }
             return false;
-        }
-        
-        public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation, float  scale, int whoAmI) 	
-        {
-        	Texture2D texture = mod.GetTexture("StardustUltimusGlowmask");
-	        spriteBatch.Draw(texture, new Vector2(item.position.X - Main.screenPosition.X + item.width * 0.5f, item.position.Y - Main.screenPosition.Y + item.height - texture.Height * 0.5f + 2f), new Rectangle(0, 0, texture.Width, texture.Height), Color.White, rotation, texture.Size() * 0.5f, scale, SpriteEffects.None, 0f);
         }
 
         public override void AddRecipes()

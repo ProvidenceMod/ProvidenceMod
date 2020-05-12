32477ae304595878a9cff19c3806d1cb6565efea
ZackFox0 <zackhemmingwayfox@gmail.com>
2020-05-12 02:14:12 -0500

    Adding Sprites NameSpace

---
+++ Code/Items/Armor/CosmicArmor/Sprites/Sprites.cs
@@ -0,0 +1,11 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace UnbiddenMod.Code.Items.Armor.CosmicArmor.Sprites
+{
+  public class Sprites {
+    
+  }
+}

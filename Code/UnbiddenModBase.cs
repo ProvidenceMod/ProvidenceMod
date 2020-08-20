28ca3894525b4d654963fcb53e65a4e6cbd77d48
SnowyStarfall <65053311+SnowyStarfall@users.noreply.github.com>
2020-08-20 03:35:26 -0500

    Several sprites added.

---
+++ Code/UnbiddenModBase.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using TAPI;
+using Terraria;
+
+namespace UnbiddenMod.UnbiddenModBase
+{
+    public class Modbase : ModBase //Use ModBase instead of Items.
+    {
+        public override void OnLoad() //This will call the code when you load up the game.
+        {
+            
+        }
+    }
+}

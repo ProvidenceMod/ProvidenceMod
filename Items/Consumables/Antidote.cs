657543d0a3e2742c210c216813ea1c6dd5a4108b
Zack <zackhemmingwayfox@gmail.com>
2020-09-17 23:52:33 -0500

    Made curative "potion"

--- Items/Consumables/Antidote.cs
+++ Items/Consumables/Antidote.cs
@@ -6,53 +6,63 @@ using UnbiddenMod.Items.Materials;
 
 namespace UnbiddenMod.Items.Consumables
 {
-    public class Antidote : ModItem
+  public class Antidote : ModItem
+  {
+
+    public override void SetStaticDefaults()
     {
-
-        public override void SetStaticDefaults()
-        {
-            DisplayName.SetDefault("Antidote");
-            Tooltip.SetDefault("Cures \"Posioned\" and \"Venom\" debuffs\nAdds 5 seconds of potion sickness\n15 HP restored");
-        }
-
-        public override void SetDefaults()
-        {
-            item.CloneDefaults(ItemID.LesserHealingPotion);
-            item.maxStack = 30;
-            item.potion = false;
-            item.healLife = 15;
-
-        }
-
-        public override bool CanUseItem(Player player) {
-          return player.FindBuffIndex(20) != -1 || player.FindBuffIndex(70) != -1; // Poison or Venom
-        }
-
-        public override bool UseItem(Player player)
-        {
-          int poison = player.FindBuffIndex(20), venom = player.FindBuffIndex(70);
-          Main.debuff[20] = false;
-          Main.debuff[70] = false;
-          if (poison != -1) {player.DelBuff(poison);}
-          if (venom != -1) {player.DelBuff(venom);}
-          player.AddBuff(21, 300, true); // Potion Sickness
-          Main.debuff[20] = true;
-          Main.debuff[70] = true;
-          return true;
-        }
-        public override void AddRecipes()
-        {
-            // Recipes here. See Basic Recipe Guide2
-            ModRecipe recipe = new ModRecipe(mod);
-
-            recipe.AddIngredient(ItemID.FragmentSolar, 15);
-            recipe.AddIngredient(ItemID.FragmentNebula, 15);
-            recipe.AddIngredient(ItemID.FragmentVortex, 15);
-            recipe.AddIngredient(ItemID.FragmentStardust, 15);
-            recipe.AddIngredient(ItemType<LuminousFragment>(), 5);
-            recipe.AddTile(412); // Ancient Manipulator
-            recipe.SetResult(this); //Sets the result of this recipe to this item
-            recipe.AddRecipe(); //Adds the recipe to the mod
-        }
+      DisplayName.SetDefault("Antidote");
+      Tooltip.SetDefault("Cures \"Posioned\" and \"Venom\" debuffs\nAdds 5 seconds of potion sickness\n15 HP restored");
     }
+
+    public override void SetDefaults()
+    {
+      item.CloneDefaults(ItemID.LesserHealingPotion);
+      item.maxStack = 30;
+      item.healLife = 15;
+      item.potion = false;
+
+    }
+
+    public override bool CanUseItem(Player player)
+    {
+      return player.FindBuffIndex(20) != -1 || player.FindBuffIndex(70) != -1; // Poison or Venom
+    }
+
+    public override bool UseItem(Player player)
+    {
+      int poison = player.FindBuffIndex(20), venom = player.FindBuffIndex(70), potionSick = player.FindBuffIndex(21);
+      Main.debuff[20] = false;
+      Main.debuff[70] = false;
+      int sickTime = Main.expertMode ? 600 : 300; // 5 seconds in normal mode, 10 in Expert
+      
+      if (potionSick != -1)
+      {
+        player.buffTime[potionSick] += sickTime;
+      }
+      else
+      {
+        player.AddBuff(21, sickTime, true);
+      } // Potion Sickness
+      if (poison != -1) { player.DelBuff(poison); }
+      if (venom != -1) { player.DelBuff(venom); }
+      Main.debuff[20] = true;
+      Main.debuff[70] = true;
+      return true;
+    }
+    public override void AddRecipes()
+    {
+      // Recipes here. See Basic Recipe Guide2
+      ModRecipe recipe = new ModRecipe(mod);
+
+      recipe.AddIngredient(ItemID.FragmentSolar, 15);
+      recipe.AddIngredient(ItemID.FragmentNebula, 15);
+      recipe.AddIngredient(ItemID.FragmentVortex, 15);
+      recipe.AddIngredient(ItemID.FragmentStardust, 15);
+      recipe.AddIngredient(ItemType<LuminousFragment>(), 5);
+      recipe.AddTile(412); // Ancient Manipulator
+      recipe.SetResult(this); //Sets the result of this recipe to this item
+      recipe.AddRecipe(); //Adds the recipe to the mod
+    }
+  }
 }

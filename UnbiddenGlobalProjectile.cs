b9939624a721f75428fd8ea60da6ea6f12276a26
SnowyStarfall <65053311+SnowyStarfall@users.noreply.github.com>
2020-09-20 05:47:18 -0500

    Added positioning to Fire Ancient AI, changed ELEMS to floats

--- UnbiddenGlobalProjectile.cs
+++ UnbiddenGlobalProjectile.cs
@@ -6,9 +6,42 @@ using Terraria.ModLoader;
 
 namespace UnbiddenMod
 {
-    public class UnbiddenGlobalProjectile : GlobalProjectile
+  public class UnbiddenGlobalProjectile : GlobalProjectile
   {
-    public override bool InstancePerEntity => true;
+
+    // Elemental variables for Projectiles
+
     public int element = -1; // -1 means Typeless, meaning we don't worry about this in the first place
+    
+
+    // Elemental variables also contained within GlobalItem, GlobalNPC, and Player
+    public override bool InstancePerEntity => true;
+
+    public static void AfterImage(Projectile projectile, Color lightColor, Texture2D texture)
+    {
+      int height = texture.Height / (int) Main.projFrames[projectile.type];
+      int y = height * projectile.frame;
+      float rotation = projectile.rotation;
+      Rectangle rectangle = new Rectangle(0, y, texture.Width, height);
+      Vector2 origin = Utils.Size(rectangle) / 2f;
+      for (int i = projectile.oldPos.Length - 1; i > 0; i--)
+      {
+        projectile.oldPos[i] = projectile.oldPos[i - 1];
+      }
+      projectile.oldPos[0] = projectile.position;
+      for (int k = 0; k < 3; k++) 
+      {
+				Vector2 previous = projectile.position;
+				if (k > 0) 
+        {
+					previous = projectile.oldPos[k - 1];
+				}
+        float alpha = 0.5f - (k * 0.15f);
+        Color color = projectile.GetAlpha(lightColor) * alpha;
+        Vector2 drawPos = projectile.oldPos[k] - Main.screenPosition;
+				Main.spriteBatch.Draw(texture, drawPos, rectangle, color, rotation, origin, 1f, SpriteEffects.None, 0f);
+      }
+      Player player = Main.player[projectile.owner];
+    }
   }
 }

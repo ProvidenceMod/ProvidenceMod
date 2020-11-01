8c4691267fe8b13840d55607922078630282d202
Zack <zackhemmingwayfox@gmail.com>
2020-10-31 21:29:15 -0400

    Adding vanilla npc support

--- NPCs/FireAncient/FireAncient.cs
+++ NPCs/FireAncient/FireAncient.cs
@@ -32,7 +32,7 @@ namespace UnbiddenMod.NPCs.FireAncient
 
         public override void SetDefaults()
         {
-            music = mod.GetSoundSlot(SoundType.Music, "Sounds/Music/FromTheDepths");
+            music = mod.GetSoundSlot(SoundType.Music, "Sounds/Music/HandsOfASlayer");
 			musicPriority = MusicPriority.BossMedium; // By default, musicPriority is BossLow
             npc.damage = 75;
             npc.aiStyle = -1;

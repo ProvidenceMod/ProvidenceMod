5d7565771a7e7712a7c89b7ac952ad001717404a
SnowyStarfall <65053311+SnowyStarfall@users.noreply.github.com>
2021-01-16 05:43:56 -0600

    Added Zephyrium ore

---
+++ Items/Placeable/ZephyriumOre.cs
@@ -0,0 +1,28 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace UnbiddenMod.Items.Placeable
+{
+	public class ZephyriumOre : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			ItemID.Sets.SortingPriorityMaterials[item.type] = 58;
+		}
+
+		public override void SetDefaults()
+		{
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.useTurn = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.autoReuse = true;
+			item.maxStack = 999;
+			item.consumable = true;
+			item.createTile = ModContent.TileType<Tiles.ZephyriumOre>();
+			item.width = 12;
+			item.height = 12;
+			item.value = 3000;
+		}
+	}
+}
---
+++ Tiles/ZephyriumOre.cs
@@ -0,0 +1,33 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace UnbiddenMod.Tiles
+{
+	public class ZephyriumOre : ModTile
+	{
+		public override void SetDefaults()
+		{
+			TileID.Sets.Ore[Type] = true;
+			Main.tileSpelunker[Type] = true; // The tile will be affected by spelunker highlighting
+			Main.tileValue[Type] = 410; // Metal Detector value, see https://terraria.gamepedia.com/Metal_Detector
+			Main.tileShine2[Type] = true; // Modifies the draw color slightly.
+			Main.tileShine[Type] = 300; // How often tiny dust appear off this tile. Larger is less frequently
+			Main.tileMergeDirt[Type] = true;
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("ZephyriumOre");
+			AddMapEntry(new Color(152, 171, 198), name);
+
+			dustType = 84;
+			drop = ModContent.ItemType<Items.Placeable.ZephyriumOre>();
+			soundType = SoundID.Tink;
+			soundStyle = 1;
+			//mineResist = 4f;
+			//minPick = 200;
+		}
+	}
+}

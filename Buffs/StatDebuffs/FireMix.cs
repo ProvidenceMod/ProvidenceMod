f3d61b2cf0bde967ff62a3a41348fec907867ed3
FloofLeo <FloofLeo@users.noreply.github.com>
2021-01-24 01:07:38 +0000

    Added 2 new Items, updated BluSMG code
    - Added temp file for FireMix, placeholder till it can be turned into a custom debuff.
    
    - BluSMG updated with (non-working) OnHit code, plus misc item changes to the damage and use time.
    
    - Added GreSMG and RedSMG.
    
    - GreSMG is a Hardmode Varient of BluSMG, but has a seperate crafting recipie.
    
    -RedSMG uses both BluSMG and GreSMG as pre-requisites + souls for a post mech boss weapon.

---
+++ Buffs/StatDebuffs/FireMix.cs
@@ -0,0 +1,18 @@
+// using Terraria;
+// using Terraria.ModLoader;
+
+// namespace UnbiddenMod.Buffs.StatDebuffs
+// {
+//   public class FireMix : ModBuff
+//   {
+//     public override void SetDefaults()
+//     {
+//       DisplayName.SetDefault("FireMix");
+//       Description.SetDefault("Four Way Inferno!");
+//       Main.debuff[Type] = true;
+//       Main.buffNoSave[Type] = true;
+//       longerExpertDebuff = false;
+//     }
+//   }
+// }
+//under comment for now until i understand how to make a custom buff/debuff
--- Items/Weapons/Ranged/GreSMG.cs
+++ Items/Weapons/Ranged/GreSMG.cs
@@ -7,13 +7,13 @@ using static Terraria.ModLoader.ModContent;
 
 namespace UnbiddenMod.Items.Weapons.Ranged
 {
-  public class BluSMG : ModItem
+  public class GreSMG : ModItem
   {
 
     public override void SetStaticDefaults()
     {
-      DisplayName.SetDefault("BluSMG");
-      Tooltip.SetDefault("\"What did you expect smashing a bunch of scrap together?\"");  
+      DisplayName.SetDefault("GreSMG");
+      Tooltip.SetDefault("\"Wait, this doesn't even fit!\"");
     }
 
     public override void SetDefaults()
@@ -21,21 +21,21 @@ namespace UnbiddenMod.Items.Weapons.Ranged
       item.CloneDefaults(ItemID.Minishark);
       item.useAmmo = AmmoID.Bullet;
       item.ranged = true;
-      item.damage = 20;
+      item.damage = 40;
       item.scale = 1f;
-      item.useTime = 6;
-      item.useAnimation = 6;
+      item.useTime = 5;
+      item.useAnimation = 5;
       item.reuseDelay = 3;
       item.autoReuse = true;
       item.knockBack = 2;
       item.width = 30;
       item.height = 16;
-      item.rare = ItemRarityID.Blue;
+      item.rare = ItemRarityID.Green;
     }
 
     public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
     {
-      Vector2 speed = new Vector2(speedX, speedY).RotatedByRandom(9f.InRadians());
+      Vector2 speed = new Vector2(speedX, speedY).RotatedByRandom(7f.InRadians());
       speedX = speed.X; speedY = speed.Y;
       return true;
     }
@@ -44,34 +44,44 @@ namespace UnbiddenMod.Items.Weapons.Ranged
     {
       return new Vector2(-16, 4);
     }
-
-
     public override void AddRecipes()
     {
       ModRecipe recipe = new ModRecipe(mod);
-      recipe.AddIngredient(ItemID.FlintlockPistol, 1);
-      recipe.AddIngredient(ItemID.Gel, 50);
+      recipe.AddIngredient(ItemID.TitaniumBar, 10);
+      recipe.AddIngredient(ItemID.CursedFlame, 15);
       recipe.AddIngredient(ItemID.IllegalGunParts, 1);
-      recipe.AddIngredient(ItemID.LeadBar, 10);
-      recipe.AddIngredient(ItemID.IceBlock, 25);
-      recipe.AddTile(TileID.Anvils);
+      recipe.AddTile(TileID.MythrilAnvil);
       recipe.SetResult(this);
       recipe.AddRecipe();
 
       ModRecipe recipe2 = new ModRecipe(mod);
-      recipe2.AddIngredient(ItemID.FlintlockPistol, 1);
-      recipe2.AddIngredient(ItemID.Gel, 50);
+      recipe2.AddIngredient(ItemID.TitaniumBar, 10);
+      recipe2.AddIngredient(ItemID.Ichor, 15);
       recipe2.AddIngredient(ItemID.IllegalGunParts, 1);
-      recipe2.AddIngredient(ItemID.IronBar, 10);
-      recipe2.AddIngredient(ItemID.IceBlock, 25);
-      recipe2.AddTile(TileID.Anvils);
+      recipe2.AddTile(TileID.MythrilAnvil);
       recipe2.SetResult(this);
       recipe2.AddRecipe();
+
+      ModRecipe recipe3 = new ModRecipe(mod);
+      recipe3.AddIngredient(ItemID.AdamantiteBar, 10);
+      recipe3.AddIngredient(ItemID.CursedFlame, 15);
+      recipe3.AddIngredient(ItemID.IllegalGunParts, 1);
+      recipe3.AddTile(TileID.MythrilAnvil);
+      recipe3.SetResult(this);
+      recipe3.AddRecipe();
+
+      ModRecipe recipe4 = new ModRecipe(mod);
+      recipe4.AddIngredient(ItemID.AdamantiteBar, 10);
+      recipe4.AddIngredient(ItemID.Ichor, 15);
+      recipe4.AddIngredient(ItemID.IllegalGunParts, 1);
+      recipe4.AddTile(TileID.MythrilAnvil);
+      recipe4.SetResult(this);
+      recipe4.AddRecipe();
     }
     public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
     {
-      target.AddBuff(BuffID.Frostburn, 240);
+      target.AddBuff(BuffID.CursedInferno, 480);
     }
   }
 }
-//pre-wof alternative to minishark, may need tweaking.
+//Intended to be Pre-Mech-Boss, potentially post if needed.

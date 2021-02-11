76266db98d6329d7125cd38ef791b552f802397b
Zack <zackhemmingwayfox@gmail.com>
2021-02-11 03:41:44 -0600

    Numbers tweaking

--- Prefixes/ElementalPrefix.cs
+++ Prefixes/ElementalPrefix.cs
@@ -19,37 +19,37 @@ namespace ProvidenceMod.Prefixes
     {
       if (!base.Autoload(ref name)) return false;
 
-      mod.AddPrefix("Burning", new ElementalPrefix(6, ElementID.Fire));
-      mod.AddPrefix("Searing", new ElementalPrefix(12, ElementID.Fire));
-      mod.AddPrefix("Scorching", new ElementalPrefix(18, ElementID.Fire));
+      mod.AddPrefix("Burning", new ElementalPrefix(10, ElementID.Fire));
+      mod.AddPrefix("Searing", new ElementalPrefix(20, ElementID.Fire));
+      mod.AddPrefix("Scorching", new ElementalPrefix(30, ElementID.Fire));
 
-      mod.AddPrefix("Chilling", new ElementalPrefix(6, ElementID.Ice));
-      mod.AddPrefix("Freezing", new ElementalPrefix(12, ElementID.Ice));
-      mod.AddPrefix("Frostbiting", new ElementalPrefix(18, ElementID.Ice));
+      mod.AddPrefix("Chilling", new ElementalPrefix(10, ElementID.Ice));
+      mod.AddPrefix("Freezing", new ElementalPrefix(20, ElementID.Ice));
+      mod.AddPrefix("Frostbiting", new ElementalPrefix(30, ElementID.Ice));
 
-      mod.AddPrefix("Sparking", new ElementalPrefix(6, ElementID.Lightning));
-      mod.AddPrefix("Shocking", new ElementalPrefix(12, ElementID.Lightning));
-      mod.AddPrefix("Electrocuting", new ElementalPrefix(18, ElementID.Lightning));
+      mod.AddPrefix("Sparking", new ElementalPrefix(10, ElementID.Lightning));
+      mod.AddPrefix("Shocking", new ElementalPrefix(20, ElementID.Lightning));
+      mod.AddPrefix("Electrocuting", new ElementalPrefix(30, ElementID.Lightning));
 
-      mod.AddPrefix("Dampening", new ElementalPrefix(6, ElementID.Water));
-      mod.AddPrefix("Dousing", new ElementalPrefix(12, ElementID.Water));
-      mod.AddPrefix("Drenching", new ElementalPrefix(18, ElementID.Water));
+      mod.AddPrefix("Dampening", new ElementalPrefix(10, ElementID.Water));
+      mod.AddPrefix("Dousing", new ElementalPrefix(20, ElementID.Water));
+      mod.AddPrefix("Drenching", new ElementalPrefix(30, ElementID.Water));
 
-      mod.AddPrefix("Cracking", new ElementalPrefix(6, ElementID.Earth));
-      mod.AddPrefix("Crushing", new ElementalPrefix(12, ElementID.Earth));
-      mod.AddPrefix("Shattering", new ElementalPrefix(18, ElementID.Earth));
+      mod.AddPrefix("Cracking", new ElementalPrefix(10, ElementID.Earth));
+      mod.AddPrefix("Crushing", new ElementalPrefix(20, ElementID.Earth));
+      mod.AddPrefix("Shattering", new ElementalPrefix(30, ElementID.Earth));
 
-      mod.AddPrefix("Breezy", new ElementalPrefix(6, ElementID.Air));
-      mod.AddPrefix("Gale-Force", new ElementalPrefix(12, ElementID.Air));
-      mod.AddPrefix("Tempestuous", new ElementalPrefix(18, ElementID.Air));
+      mod.AddPrefix("Breezy", new ElementalPrefix(10, ElementID.Air));
+      mod.AddPrefix("Gale-Force", new ElementalPrefix(20, ElementID.Air));
+      mod.AddPrefix("Tempestuous", new ElementalPrefix(30, ElementID.Air));
 
-      mod.AddPrefix("Pious", new ElementalPrefix(6, ElementID.Radiant));
-      mod.AddPrefix("Cleansing", new ElementalPrefix(12, ElementID.Radiant));
-      mod.AddPrefix("Purging", new ElementalPrefix(18, ElementID.Radiant));
+      mod.AddPrefix("Pious", new ElementalPrefix(10, ElementID.Radiant));
+      mod.AddPrefix("Cleansing", new ElementalPrefix(20, ElementID.Radiant));
+      mod.AddPrefix("Purging", new ElementalPrefix(30, ElementID.Radiant));
 
-      mod.AddPrefix("Afflicting", new ElementalPrefix(6, ElementID.Necrotic));
-      mod.AddPrefix("Pestilent", new ElementalPrefix(12, ElementID.Necrotic));
-      mod.AddPrefix("Abhorrent", new ElementalPrefix(18, ElementID.Necrotic));
+      mod.AddPrefix("Afflicting", new ElementalPrefix(10, ElementID.Necrotic));
+      mod.AddPrefix("Pestilent", new ElementalPrefix(20, ElementID.Necrotic));
+      mod.AddPrefix("Abhorrent", new ElementalPrefix(30, ElementID.Necrotic));
       return false;
     }
     public override void SetStats(ref float damageMult, ref float knockbackMult, ref float useTimeMult, ref float scaleMult, ref float shootSpeedMult, ref float manaMult, ref int critBonus)

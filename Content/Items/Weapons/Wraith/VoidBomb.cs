5e83e7504af102043aa32e199773782ee241dec7
Zack <kindthesixth@yahoo.com>
2022-03-17 13:00:27 -0500

    Made Void Bomb Functional

--- Content/Items/Weapons/Wraith/VoidBomb.cs
+++ Content/Items/Weapons/Wraith/VoidBomb.cs
@@ -4,7 +4,9 @@ using Providence.Rarities;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Providence.DamageClasses;
 using static Terraria.ModLoader.ModContent;
+using Terraria.DataStructures;
 
 namespace Providence.Content.Items.Weapons.Wraith
 {
@@ -20,8 +22,8 @@ namespace Providence.Content.Items.Weapons.Wraith
 			Item.damage = 50;
 			Item.width = 90;
 			Item.height = 90;
-			Item.useTime = 10;
-			Item.useAnimation = 10;
+			Item.useTime = 30;
+			Item.useAnimation = 30;
 			Item.useStyle = ItemUseStyleID.Swing;
 			Item.DamageType = DamageClass.Throwing;
 			Item.shoot = ProjectileType<Projectiles.Wraith.VoidBomb>();
@@ -32,6 +34,7 @@ namespace Providence.Content.Items.Weapons.Wraith
 			Item.noMelee = true;
 			Item.consumable = true;
 			Item.rare = RarityType<Developer>();
+			Item.damage = 50;
 		}
 		public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
 		{
@@ -40,6 +43,12 @@ namespace Providence.Content.Items.Weapons.Wraith
 		public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
 		{
 			return true;
+		}
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+		{
+			for (int i = 0; i < 3; i++)
+				_ = Projectile.NewProjectile(source, position, velocity.RotatedByRandom(10f.InRadians()), type, damage, knockback);
+			return false;
 		}
 	}
 }
--- Content/Projectiles/Wraith/VoidBomb.cs
+++ Content/Projectiles/Wraith/VoidBomb.cs
@@ -2,6 +2,9 @@
 using System;
 using Terraria;
 using Terraria.ModLoader;
+using Terraria.ID;
+using Providence.Globals.Systems.Particles;
+using ParticleLibrary;
 
 namespace Providence.Content.Projectiles.Wraith
 {
@@ -22,16 +25,20 @@ namespace Providence.Content.Projectiles.Wraith
 			Projectile.ignoreWater = true;
 			Projectile.timeLeft = 3f.InTicks();
 			Projectile.penetrate = 1;
-			//projectile.damage = 50; A soft damage, don't want the proj itself dealing damage
+			Projectile.damage = 50;
 		}
 		public override void AI()
 		{
-			Projectile.velocity.Y += 0.3f;
+			if (Projectile.ai[0] > 30f)
+				Projectile.velocity.Y += 0.3f;
 			if (Projectile.velocity.Y > 16f)
 				Projectile.velocity.Y = 16f;
-			Projectile.ai[0]++;
+			Projectile.ai[0] += 1f;
 		}
-		public override bool PreDraw(ref Color color) => false;
+		public override bool PreDraw(ref Color color) {
+			ParticleManager.NewParticle(Projectile.Center, new Vector2(Main.rand.NextFloat(-1f, 2f), Main.rand.NextFloat(-1f, 2f)), new Metaball(), Color.White, 0.2f);
+			return false;
+		}
 		public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
 		{
 			contactTarget = target;
@@ -39,21 +46,24 @@ namespace Providence.Content.Projectiles.Wraith
 			Projectile.Kill();
 			Projectile.active = false;
 		}
-		public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox) => projHitbox.Intersects(targetHitbox);
 		public override void Kill(int timeLeft)
 		{
-			for (byte i = 0; i < 15; i++)
+			for (byte i = 0; i < 30; i++)
 			{
 				Vector2 randPointWI100f = Projectile.Center;
 				randPointWI100f.X += Main.rand.NextFloat(-50f, 50f);
 				randPointWI100f.Y += Main.rand.NextFloat(-50f, 50f);
+				ParticleManager.NewParticle(randPointWI100f, new Vector2(Main.rand.NextFloat(-3f, 4f), Main.rand.NextFloat(-3f, 4f)), new Metaball(), Color.White, Main.rand.NextFloat(0.5f, 1f));
+				Lighting.AddLight(randPointWI100f, new Vector3(0.5f, 0.5f, 0.5f));
 			}
 			Player owner = Projectile.OwnerPlayer();
 			foreach (NPC npc in Main.npc)
 			{
-				if (npc.active && !npc.townNPC && npc.Center.IsInRadiusOf(Projectile.position, 100f) && npc != contactTarget)
+				if (npc.active && !npc.townNPC && 
+					npc.type != NPCID.DD2EterniaCrystal && npc.type != NPCID.DD2LanePortal &&
+					npc.Center.IsInRadiusOf(Projectile.position, 100f) && npc != contactTarget)
 				{
-					npc.StrikeNPC(50 + (int)Math.Floor((decimal)(timeLeft / 10)), 1f, -npc.direction, owner.GetCritChance(DamageClass.Throwing).PercentChance());
+					_ = npc.StrikeNPC(50, 1f, -npc.direction, owner.GetCritChance(DamageClass.Throwing).PercentChance());
 				}
 			}
 		}
